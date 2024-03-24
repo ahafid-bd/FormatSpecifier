0f7192b15834a1e97f323aa62c1842dcf2389ca5
Ahafid <ahafidkabir11@gmail.com>
2024-03-24 12:35:22 +0600

    Add files via upload

---
+++ FormatSpecifier/src/formatspecifier/FormatSpecifier.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formatspecifier;
+
+public class FormatSpecifier {
+    
+    public static void main(String[] args) {
+       boolean b = true;
+       char c ='a';
+       float f = 2.00f;
+       int i = 3;
+        System.out.printf("boolean is = %b \n",b);
+        System.out.printf("char is = %c \n",c);
+        System.out.printf("float is = %.1f \n",f);
+        System.out.printf("int is = %d\n",i);
+        
+        
+        
+    }
+    
+}
